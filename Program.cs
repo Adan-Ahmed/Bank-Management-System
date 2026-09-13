ecb4ea35ed8bb367e50cb78d7a20207f702273d6
Adan-Ahmed <adan.ahmed.work@gmail.com>
2026-09-13 17:56:50 +0500

    update

--- Program.cs
+++ Program.cs
@@ -65,10 +65,15 @@ class Account
         return AccountBalance;
     }
 }
+
+// inheriance
 class SavingsAccount : Account 
-{ 
-
-
+{
+    public SavingsAccount(int accountnumber, string accountname, double accountbalance)
+        : base(accountnumber, accountname, accountbalance) 
+    {
+    
+    }
 }
 
 class Program
