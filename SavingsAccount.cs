37e56a4ed714fc575b47f2f0621a2bbaa40f9c08
Adan-Ahmed <adan.ahmed.work@gmail.com>
2026-09-13 19:15:09 +0500

    update

--- SavingsAccount.cs
+++ SavingsAccount.cs
@@ -1,10 +1,17 @@
 ﻿public class SavingsAccount : Account
 {
+    public double InterestRate {  get; set; }
     public SavingsAccount(
         int accountnumber,
         string accountname,
         double accountbalance)
         : base(accountnumber, accountname, accountbalance)
     {
+        InterestRate = interestrate;
+    }
+    public void AddInterest() 
+    {
+        double interest = AccountBalance * InterestRate / 100;
+        AccountBalance += interest;
     }
 }
